d0a0499a126ec22856769aff4676595f713a4eec
Raghvendra-eng <81280239+Raghvendra-eng@users.noreply.github.com>
2021-03-25 10:06:42 +0530

    Update AccountServiceClass.java

--- account/src/main/java/com/user/account/services/AccountServiceClass.java
+++ account/src/main/java/com/user/account/services/AccountServiceClass.java
@@ -1,16 +1,21 @@
 package com.user.account.services;
 
+//import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+//import com.user.account.entity.Transactions;
 import com.user.account.entity.User;
 import com.user.account.repo.RepoLayer;
+//import com.user.account.repo.TransactionRepoLayerInFace;
 
 @Service
 public class AccountServiceClass implements AccountServices {
 
 	@Autowired
 	private RepoLayer repoLayerVari;
+	
 	
 	@Override
 	public User updateUser(User user) {
@@ -22,5 +27,6 @@ public class AccountServiceClass implements AccountServices {
 		// TODO Auto-generated method stub
 		return repoLayerVari.getOne(accountNumber);
 	}
+
 
 }
