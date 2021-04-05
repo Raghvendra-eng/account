ff2aa585dbdf6980af9dff150893fe8bc1d5c540
Raghvendra-eng <abhishek.pandey6150@gmail.com>
2021-04-05 10:44:45 +0530

    User not found exception and redundancy issue fixes

--- account/src/main/java/com/user/account/controller/AllControlers.java
+++ account/src/main/java/com/user/account/controller/AllControlers.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import javax.validation.constraints.Size;
+//import javax.validation.constraints.Size;
 
 import com.user.account.entity.User;
 import com.user.account.message.DefaultMessage;
@@ -19,78 +19,74 @@ import com.user.account.entity.Transactions;
 import com.user.account.services.AccountService;
 import com.user.account.services.TransactionService;
 
+import javax.persistence.EntityNotFoundException;
+import javax.validation.constraints.Min;
+
 @Validated
 @RestController
 public class AllControlers {
 
 
-	@Autowired
-	private AccountService accountService;
+    @Autowired
+    private AccountService accountService;
 
-	@Autowired
-	private TransactionService transactionService;
+    @Autowired
+    private TransactionService transactionService;
 
-	// Updating accountBalance after debit
+    // Updating accountBalance after debit
+    public ResponseEntity<String> CreditAndDebitRequest(Long accountNumber, String transactionAmount, String transactionType){
+        try {
+            User user = accountService.getUser(accountNumber);
+            Long amount = Long.parseLong(transactionAmount);
+            if(transactionType.equals(DefaultMessage.DEBIT) ) {
+                if ( amount.compareTo(user.getAccountBalance()) <= 0){
+                    user.incrementAccountBalance(-amount);
+                    Transactions newTransaction =
+                            new Transactions(accountNumber, amount, DefaultMessage.DEBIT);
+                    transactionService.addTransaction(newTransaction);
+                    accountService.updateUser(user);
+                    return new ResponseEntity<>(DefaultMessage.TRANSACTION_SUCCESS, HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<>(DefaultMessage.TRANSACTION_FAILED_LOW_BALANCE, HttpStatus.BAD_REQUEST);
+                }
+            }
+            else{
+                user.incrementAccountBalance(amount) ;
+                Transactions newTransaction =
+                        new Transactions(accountNumber, amount, DefaultMessage.CREDIT);
+                transactionService.addTransaction(newTransaction);
+                accountService.updateUser(user);
+                return new ResponseEntity<>(DefaultMessage.TRANSACTION_SUCCESS, HttpStatus.OK);
+            }
 
+        }
+        catch (NumberFormatException exception)
+        {
+            return new ResponseEntity<>(DefaultMessage.TRANSACTION_FAILED_INVALID_AMOUNT, HttpStatus.BAD_REQUEST);
+        }
+        catch (EntityNotFoundException exception)
+        {
+            return new ResponseEntity<>(DefaultMessage.INVALID_ACCOUNT_NUMBER, HttpStatus.BAD_REQUEST);
+        }
+    }
 
-	@PostMapping("/debit/{accountNumber}")
-	public ResponseEntity<String> debit(
-			@PathVariable @Size(10) Long accountNumber, 
-			@RequestBody String transactionAmount) {
-		try {
+    @PostMapping("/debit/{accountNumber}")
+    public ResponseEntity<String> debit(@PathVariable @Min(1) Long accountNumber, @RequestBody String transactionAmount) {
+        return CreditAndDebitRequest(accountNumber, transactionAmount, DefaultMessage.DEBIT);
+    }
 
-			User user = accountService.getUser(accountNumber);
-			Long amount = Long.parseLong(transactionAmount);
-			if ( amount.compareTo(user.getAccountBalance()) <= 0 ) {
-				user.incrementAccountBalance(-amount) ;
-				Transactions newTransaction = 
-						new Transactions(accountNumber, amount, DefaultMessage.DEBIT);
-				transactionService.addTransaction(newTransaction);
-				accountService.updateUser(user);
-				return new ResponseEntity<>(DefaultMessage.TRANSACTION_SUCCESS, HttpStatus.OK);
-			}
+    // Updating accountBalance after credit
 
-			else {
-				return new ResponseEntity<>(DefaultMessage.TRANSACTION_FAILED_LOW_BALANCE, HttpStatus.BAD_REQUEST);
-			}
-			
-		}
-		catch (NumberFormatException exception) {
+    @PostMapping("/credit/{accountNumber}")
+    public ResponseEntity<String> credit(@PathVariable Long accountNumber, @RequestBody String transactionAmount) throws Exception{
+        return CreditAndDebitRequest(accountNumber, transactionAmount, DefaultMessage.CREDIT);
+    }
 
-			return new ResponseEntity<>(DefaultMessage.TRANSACTION_FAILED_INVALID_AMOUNT, HttpStatus.BAD_REQUEST);
-		
-		}
-	}
+    // GetSummary for transactions in an account
 
-	// Updating accountBalance after credit
+    @GetMapping("/getSummary/{accountNumber}")
 
-	@PostMapping("/credit/{accountNumber}")
-	public ResponseEntity<String> credit(@PathVariable Long accountNumber, @RequestBody String transactionAmount) throws Exception{
-		try {
-		
-			User user = accountService.getUser(accountNumber);
-			Long amount = Long.parseLong(transactionAmount);
-			user.incrementAccountBalance(amount) ;
-			Transactions newTransaction = 
-					new Transactions(accountNumber, amount, DefaultMessage.CREDIT);
-			transactionService.addTransaction(newTransaction);
-			accountService.updateUser(user);
-			return new ResponseEntity<>(DefaultMessage.TRANSACTION_SUCCESS, HttpStatus.OK);
-		
-		}
-		catch (NumberFormatException exception) {
-
-			return new ResponseEntity<>(DefaultMessage.TRANSACTION_FAILED_INVALID_AMOUNT, HttpStatus.BAD_REQUEST);
-		
-		}
-	}
-
-	// GetSummary for transactions in an account
-
-	@GetMapping("/getSummary/{accountNumber}")
-
-	public ArrayList < Transactions > transactionSummary(@PathVariable Long accountNumber){
-
-		return transactionService.getSummary(accountNumber);
-	}
+    public ArrayList < Transactions > transactionSummary(@PathVariable Long accountNumber){
+        return transactionService.getSummary(accountNumber);
+    }
 }
