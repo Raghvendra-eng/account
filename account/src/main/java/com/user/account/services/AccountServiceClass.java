e6f0f88e4c1bf05b2f5aef47bd7f4aaf05151dd2
Raghvendra-eng <abhishek.pandey6150@gmail.com>
2021-04-07 12:56:12 +0530

    code hygiene

--- account/src/main/java/com/user/account/services/AccountServiceClass.java
+++ account/src/main/java/com/user/account/services/AccountServiceClass.java
@@ -1,7 +1,5 @@
 package com.user.account.services;
 
-//import java.util.ArrayList;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.user.account.entity.User;
