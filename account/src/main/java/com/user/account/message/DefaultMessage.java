39f9b2cd6b6bf39c2216383ec21e2f6ec57882cc
Raghvendra-eng <abhishek.pandey6150@gmail.com>
2021-04-07 12:33:58 +0530

    Typo error correction and added invalid account number message

--- account/src/main/java/com/user/account/message/DefaultMessage.java
+++ account/src/main/java/com/user/account/message/DefaultMessage.java
@@ -1,10 +1,11 @@
 package com.user.account.message;
 
 public class DefaultMessage {
-	public final static String DEBIT = "Dabit";
-	public final static String CREDIT = "Credit";
-	public final static String TRANSACTION_SUCCESS = "Transaction Successful";
-	public final static String TRANSACTION_FAILED_LOW_BALANCE = "Transaction Failed due to Low Account Balance";
-	public final static String TRANSACTION_FAILED_INVALID_AMOUNT = "Transaction Failed due to Invalid Amount";
-	public final static String MINIMUM_ACCOUNT_NUMBER = "Account Number Must be greater than 1";
+    public final static String DEBIT = "Debit";
+    public final static String CREDIT = "Credit";
+    public final static String TRANSACTION_SUCCESS = "Transaction Successful";
+    public final static String TRANSACTION_FAILED_LOW_BALANCE = "Transaction Failed due to Low Account Balance";
+    public final static String TRANSACTION_FAILED_INVALID_AMOUNT = "Transaction Failed due to Invalid Amount";
+    public final static String MINIMUM_ACCOUNT_NUMBER = "Account Number Must be greater than 1";
+    public final static String INVALID_ACCOUNT_NUMBER = "Invalid Account Number";
 }
