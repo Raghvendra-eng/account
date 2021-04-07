6ae5fa6cdc0774420d56816df0d6447bdf7fa029
Raghvendra-eng <abhishek.pandey6150@gmail.com>
2021-04-07 13:03:31 +0530

    Concurrency management using @Transactional

--- account/src/main/java/com/user/account/services/UserTransactionService.java
+++ account/src/main/java/com/user/account/services/UserTransactionService.java
@@ -1,6 +1,6 @@
 package com.user.account.services;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,21 +8,25 @@ import org.springframework.stereotype.Service;
 import com.user.account.entity.Transactions;
 import com.user.account.repositories.TransactionRepository;
 
+import javax.transaction.Transactional;
+
+
 @Service
 public class UserTransactionService implements TransactionService {
-	
-	
-	@Autowired
-	private TransactionRepository transactionRepository;
-	
-	@Override
-	public Transactions addTransaction(Transactions newTransaction) {
-		return transactionRepository.save(newTransaction);
-	}
 
-	@Override
-	public ArrayList<Transactions> getSummary(Long accountNumber) {
-		return transactionRepository.findByAccountNumber(accountNumber);
-	}
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Override
+    @Transactional
+    public Transactions addTransaction(Transactions newTransaction) {
+        return transactionRepository.save(newTransaction);
+    }
+
+    @Override
+    public List<Transactions> getSummary(Long accountNumber) {
+        return transactionRepository.findByAccountNumber(accountNumber);
+    }
 
 }
