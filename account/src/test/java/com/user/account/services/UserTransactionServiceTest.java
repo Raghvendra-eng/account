d6ae66b8cf599b8ceddf54d1134798cbb32f1b8b
Raghvendra-eng <abhishek.pandey6150@gmail.com>
2021-04-09 03:03:13 +0530

    Unit testing for transaction Service

---
+++ account/src/test/java/com/user/account/services/UserTransactionServiceTest.java
@@ -0,0 +1,43 @@
+package com.user.account.services;
+
+import com.user.account.entity.Transactions;
+import com.user.account.entity.User;
+import com.user.account.repositories.TransactionRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+public class UserTransactionServiceTest {
+    @Mock
+    private TransactionRepository transactionRepository;
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @Test
+    public void addTransactionTest() throws Exception{
+        Transactions inputTransactions = new Transactions(2, 1200, "Credit");
+        Mockito.when(transactionRepository.save(inputTransactions)).thenReturn(inputTransactions);
+        assertEquals(inputTransactions, transactionRepository.save(inputTransactions));
+    }
+
+    @Test
+    public void getTransactionSummaryTest() throws Exception{
+        List <Transactions> userTransactionSummary = new ArrayList<>();
+        userTransactionSummary.add(new Transactions(2, 1200,"Credit"));
+        userTransactionSummary.add(new Transactions(2, 1300,"Credit"));
+        Mockito.when(transactionRepository.findByAccountNumber(2L)).thenReturn(userTransactionSummary);
+        assertEquals(userTransactionSummary, transactionRepository.findByAccountNumber(2L));
+    }
+}
