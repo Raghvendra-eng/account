dd86a9e617d02fe91b982188403c67b4f47ccc8a
Raghvendra-eng <abhishek.pandey6150@gmail.com>
2021-04-15 15:58:00 +0530

    Moved HTTP status handler to controller layer. Instead of using return Response compared with boolean in the debitService and creditService

--- account/src/main/java/com/user/account/controller/AllControllers.java
+++ account/src/main/java/com/user/account/controller/AllControllers.java
@@ -1,8 +1,11 @@
 package com.user.account.controller;
+
 import java.util.List;
 import com.user.account.advice.CustomExceptions;
 import com.user.account.entity.FundTransferDetails;
+import com.user.account.message.DefaultMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -22,36 +25,43 @@ public class AllControllers {
 
     @Autowired
     private TransactionService transactionService;
+    
+    //Send the response to the user based on transaction status
+    public ResponseEntity<String> transactionResponse(boolean transactionStatus){
+        if(transactionStatus)
+            return new ResponseEntity<>(DefaultMessage.TRANSACTION_SUCCESS, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(DefaultMessage.TRANSACTION_FAILED_LOW_BALANCE, HttpStatus.BAD_REQUEST);
+    }
 
     // Updating accountBalance after debit
-
     @PostMapping("/debit/{accountNumber}")
     public ResponseEntity<String> debit(@PathVariable @Min(1) Long accountNumber, @RequestParam @Min(1) Long amount) throws CustomExceptions {
         User user = accountService.getUser(accountNumber);
-        return transactionService.debitService(user, amount);
+        boolean transactionStatus = transactionService.debitService(user, amount);
+        return transactionResponse(transactionStatus);
     }
 
     // Updating accountBalance after credit
-
     @PostMapping("/credit/{accountNumber}")
     public ResponseEntity<String> credit(@PathVariable @Min(1) Long accountNumber, @RequestParam @Min(1) Long amount) throws CustomExceptions {
         User user = accountService.getUser(accountNumber);
-        return transactionService.creditService(user, amount);
+        boolean transactionStatus = transactionService.creditService(user, amount);
+        return transactionResponse(transactionStatus);
     }
 
     // GetSummary for transactions in an account
-
     @GetMapping("/getSummary/{accountNumber}")
     public List< Transactions > transactionSummary(@PathVariable @Min(1) Long accountNumber) throws CustomExceptions {
         return transactionService.getSummary(accountNumber);
     }
 
     // Fund Transfer from one Account to another
-
     @PostMapping("/transaction")
     public ResponseEntity<String> fundTransfer(@RequestBody FundTransferDetails fundTransferDetails) throws CustomExceptions {
         User debitedUser = accountService.getUser(fundTransferDetails.getDebitUserAccountNumber());
         User creditedUser = accountService.getUser(fundTransferDetails.getCreditUserAccountNumber());
-        return transactionService.fundTransferService(debitedUser, creditedUser, fundTransferDetails.getTransactionAmount());
+        boolean transactionStatus = transactionService.fundTransferService(debitedUser, creditedUser, fundTransferDetails.getTransactionAmount());
+        return transactionResponse(transactionStatus);
     }
 }
--- account/src/main/java/com/user/account/services/serviceImplementation/UserTransactionService.java
+++ account/src/main/java/com/user/account/services/serviceImplementation/UserTransactionService.java
@@ -7,8 +7,6 @@ import com.user.account.message.DefaultMessage;
 import com.user.account.services.AccountService;
 import com.user.account.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.user.account.entity.Transactions;
 import com.user.account.repositories.TransactionRepository;
@@ -22,28 +20,6 @@ public class UserTransactionService implements TransactionService {
 
     @Autowired
     private AccountService accountService;
-
-    public ResponseEntity<String> debitImplementation(User user, Long amount){
-        if ( amount.compareTo(user.getAccountBalance()) <= 0){
-            user.decrementAccountBalance(amount);
-            Transactions newTransaction =
-                    new Transactions(user.getAccountNumber(), amount, DefaultMessage.DEBIT);
-            addTransaction(newTransaction);
-            accountService.updateUser(user);
-            return new ResponseEntity<>(DefaultMessage.TRANSACTION_SUCCESS, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(DefaultMessage.TRANSACTION_FAILED_LOW_BALANCE, HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    public ResponseEntity<String> creditImplementation(User user, Long amount){
-        user.incrementAccountBalance(amount) ;
-        Transactions newTransaction =
-                new Transactions(user.getAccountNumber(), amount, DefaultMessage.CREDIT);
-        addTransaction(newTransaction);
-        accountService.updateUser(user);
-        return new ResponseEntity<>(DefaultMessage.TRANSACTION_SUCCESS, HttpStatus.OK);
-    }
 
     @Override
     @Transactional
@@ -59,26 +35,34 @@ public class UserTransactionService implements TransactionService {
     }
 
     @Override
-    public ResponseEntity<String> debitService(User user, Long amount){
-        return debitImplementation(user, amount);
+    public boolean debitService(User user, Long amount){
+        if ( amount.compareTo(user.getAccountBalance()) <= 0){
+            user.decrementAccountBalance(amount);
+            Transactions newTransaction =
+                    new Transactions(user.getAccountNumber(), amount, DefaultMessage.DEBIT);
+            addTransaction(newTransaction);
+            accountService.updateUser(user);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
-    public ResponseEntity<String> creditService(User user, Long amount){
-        return creditImplementation(user, amount);
+    public boolean creditService(User user, Long amount){
+        user.incrementAccountBalance(amount) ;
+        Transactions newTransaction =
+                new Transactions(user.getAccountNumber(), amount, DefaultMessage.CREDIT);
+        addTransaction(newTransaction);
+        accountService.updateUser(user);
+        return true;
     }
 
     @Override
-    public ResponseEntity<String> fundTransferService(User debitedUser, User creditedUser, Long amount){
-        ResponseEntity<String> debitResponse = debitImplementation(debitedUser, amount);
-        ResponseEntity<String> returnResponse = new ResponseEntity<>(DefaultMessage.TRANSACTION_SUCCESS, HttpStatus.OK);
-        ResponseEntity<String> creditResponse;
-        if(!debitResponse.equals(returnResponse))
-            return debitResponse;
-        creditResponse = creditImplementation(creditedUser, amount);
-        if(!creditResponse.equals(returnResponse))
-            return creditResponse;
-        return returnResponse;
+    public boolean fundTransferService(User debitedUser, User creditedUser, Long amount){
+        boolean debitResponse = debitService(debitedUser, amount);
+        if(!debitResponse)
+            return false;
+        return creditService(creditedUser, amount);
     }
-
 }
